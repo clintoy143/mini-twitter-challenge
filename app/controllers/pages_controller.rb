4ea88d37645ee540154ea623f95e508cb9af0854
Nude <clintvencor@gmail.com>
2018-02-12 22:50:33 +0800

    write test suites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
-  def index
-  end
+    def index
+    end
+    
+    def contact
+    end
+    
+    def about
+    end
+    
+    def gallery
+    end
+    
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get pages_index_url
-    assert_response :success
-  end
+    
+    def setup
+        @base_title = "Review on Rails"
+    end
+    
+    test "should get index" do
+        get root_url
+        assert_response :success
+        assert_select "title", "Home | #{@base_title}"
+    end
 
+    test "should get about" do
+        get about_url
+        assert_response :success
+        assert_select "title", "About | #{@base_title}"
+    end
+    
+    test "should get contact" do
+        get contact_url
+        assert_response :success
+        assert_select "title", "Contacts | #{@base_title}"
+    end
 end
